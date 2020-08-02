d20013297db60c47aaf396e38024ce0b9e4b03b5
Carl Colglazier <carl@carlcolglazier.com>
2020-08-02 15:19:47 +0100

    Use preset

--- launch.sh
+++ launch.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 su - patch -c 'whoami'
-su - patch -c '/usr/local/pianoteq/pianoteq --multicore max --headless'
+su - patch -c '/usr/local/pianoteq/pianoteq --multicore max --headless --preset "Simple Steinway D"'
 
