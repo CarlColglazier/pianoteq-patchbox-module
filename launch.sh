b09d9cdbc858ab8cf0ff240b030a45e253e5b8b5
carl colglazier <carl@carlcolglazier.com>
2021-01-11 16:12:05 -0500

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -2,4 +2,4 @@
 
 su - patch -c 'whoami'
 su - patch -c '/usr/local/pianoteq/pianoteq --multicore max --headless --fxp "/usr/local/patchbox-modules/imported/pianoteq-patchbox-module/Simple Steinway.fxp"'
-su - patch -c 'jack_connect system:midi_capture_2 Pianoteq:midi_in"
+su - patch -c 'jack_connect system:midi_capture_2 Pianoteq:midi_in'
