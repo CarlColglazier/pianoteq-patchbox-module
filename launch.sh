2034c36d18ac362a0ef1b7528a352fc65b8b9de2
Carl Colglazier <carl@carlcolglazier.com>
2021-01-11 18:34:21 -0500

    Remove jack_connect command.

--- launch.sh
+++ launch.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-/usr/local/pianoteq/pianoteq --multicore max --headless --fxp "/usr/local/patchbox-modules/imported/pianoteq-patchbox-module/Simple Steinway.fxp" &
+/usr/local/pianoteq/pianoteq --multicore max --headless --fxp "/usr/local/patchbox-modules/imported/pianoteq-patchbox-module/Simple Steinway.fxp" 
 
-sleep 1 && jack_connect system:midi_capture_2 Pianoteq:midi_in
+#sleep 1 && jack_connect system:midi_capture_2 Pianoteq:midi_in
