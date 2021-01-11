215c56b7bc40cb7a00259d161fefc261d2cfe1c3
carl colglazier <carl@carlcolglazier.com>
2021-01-11 18:21:43 -0500

    sleep before midi connect

--- launch.sh
+++ launch.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-/usr/local/pianoteq/pianoteq --multicore max --headless --fxp "/usr/local/patchbox-modules/imported/pianoteq-patchbox-module/Simple Steinway.fxp"
-jack_connect system:midi_capture_2 Pianoteq:midi_in
+/usr/local/pianoteq/pianoteq --multicore max --headless --fxp "/usr/local/patchbox-modules/imported/pianoteq-patchbox-module/Simple Steinway.fxp" &
+
+sleep 1 && jack_connect system:midi_capture_2 Pianoteq:midi_in
