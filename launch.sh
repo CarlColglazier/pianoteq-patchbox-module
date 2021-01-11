0cf212942dba0f4d05c47ef18926f7220fc1ce53
Carl Colglazier <carl@carlcolglazier.com>
2021-01-11 18:39:16 -0500

    Run as 'patch' user.

--- launch.sh
+++ launch.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-/usr/local/pianoteq/pianoteq --multicore max --headless --fxp "/usr/local/patchbox-modules/imported/pianoteq-patchbox-module/Simple Steinway.fxp" 
+su patch -c '/usr/local/pianoteq/pianoteq --multicore max --headless --fxp "/usr/local/patchbox-modules/imported/pianoteq-patchbox-module/Simple Steinway.fxp"'
 
-#sleep 1 && jack_connect system:midi_capture_2 Pianoteq:midi_in
+su patch -c 'sleep 1 && jack_connect system:midi_capture_2 Pianoteq:midi_in'
