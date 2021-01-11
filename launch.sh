c6a2a088f5d9f3d4c2884a84c8d53b2ee36c1241
carl colglazier <carl@carlcolglazier.com>
2021-01-11 14:43:40 -0500

    Connect MIDI in.

--- launch.sh
+++ launch.sh
@@ -2,4 +2,4 @@
 
 su - patch -c 'whoami'
 su - patch -c '/usr/local/pianoteq/pianoteq --multicore max --headless --preset "Simple Steinway D"'
-
+su - patch -c 'jack_connect system:midi_capture_2 Pianoteq:midi_in"
